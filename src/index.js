47cc389a7b7f3162b323afc153413bddd206749c
Hu Yangwei <huyangwei_vendor@DOMAIN.SENSETIME.COM>
2018-12-14 15:30:31 +0800

    模块热替换--hot

--- src/index.js
+++ src/index.js
@@ -14,3 +14,10 @@ function component() {
   return element;
  }
  document.body.appendChild(component());
+
+ if(module.hot){
+   module.hot.accept('./print.js', function(){
+     console.log('Accepting the updated printMe module!');
+     printMe();
+   })
+ }
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,28 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
 module.exports = {
   entry: {
-    app: './src/index.js',
-    print: './src/print.js'
+    app: './src/index.js'
   },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
   },
   devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Output management'
     }),
-    new CleanWebpackPlugin(['dist'])
+    new CleanWebpackPlugin(['dist']),
+    new webpack.HotModuleReplacementPlugin()
   ],
   module: {
     rules: [
