4df3e7e4cfb4249d9e3f396edb7a623af6736c02
Hu Yangwei <huyangwei_vendor@DOMAIN.SENSETIME.COM>
2018-12-18 11:12:41 +0800

    lib外部组件

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import _ from 'lodash';
+// import * as transfNumbers from './components/transf-numbers';
+import { wordToNum, numToWord } from './components/transf-numbers';
 // import Print from './print';
 function component(){
     let ele = document.createElement('div');
@@ -9,3 +11,8 @@ function component(){
 }
   
 document.body.appendChild(component())
+
+// console.log(transfNumbers.wordToNum('Two'));
+// console.log(transfNumbers.numToWord(3));
+console.log(numToWord(3));
+console.log(wordToNum('Two'));
